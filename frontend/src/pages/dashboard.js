f778de8dd220702ec9688d8c66a9cf58a608415b
macsta31 <mackstathis@gmail.com>
2023-11-26 20:23:44 -0500

    dashboard page ideas added markup

--- frontend/src/pages/dashboard.js
+++ frontend/src/pages/dashboard.js
@@ -9,6 +9,18 @@ const Dashboard = () => {
 
     <p>This will be a protected route</p>
 
+    <br />
+
+    <p>Can show recent activity in channels and any new notifications that havent been read</p>
+
+    <br />
+
+    <p>Read status can be handled with read flag in schema for message</p>
+
+    <br />
+
+    <p>put a mini to do list here lmao</p>
+
     </>
   )
 }
