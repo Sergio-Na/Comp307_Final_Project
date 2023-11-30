e55e51da9418d51fd61edd6375d71d45e88aed6c
Sergio Najm <98653927+Sergio-Na@users.noreply.github.com>
2023-11-30 14:25:26 -0500

    Fixed bug with user schema

--- backend/models/userSchema.js
+++ backend/models/userSchema.js
@@ -5,6 +5,7 @@ const userSchema = new mongoose.Schema({
   lastName: { type: String, required: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
+  channels: [{ type: mongoose.Schema.Types.ObjectId, ref: "Channel" }],
   bio: String,
   profilePicture: String,
 });
