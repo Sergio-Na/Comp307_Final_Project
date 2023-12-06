c80a429a9755d0a96cf0a35d14c8ce5df56381d5
Sergio Najm <98653927+Sergio-Na@users.noreply.github.com>
2023-12-06 17:47:47 -0500

    random formatting (prettier :angry: ) but changed h1 and h2 to be more relevant

--- frontend/src/components/Main.js
+++ frontend/src/components/Main.js
@@ -3,12 +3,12 @@ import { useLocation, Routes, Route, useNavigate } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Home from "../pages";
 import About from "../pages/about";
-import styled from 'styled-components';
+import styled from "styled-components";
 import Dashboard from "../pages/dashboard";
-import SignUp from '../pages/signup.js'
+import SignUp from "../pages/signup.js";
 import SignIn from "../pages/signin.js";
-import Channels from '../pages/channels.js';
-import Channel  from '../pages/channel.js';
+import Channels from "../pages/channels.js";
+import Channel from "../pages/channel.js";
 import ProtectedRoute from "./ProtectedRoute.js";
 import Profile from "../pages/profile.js";
 import { useEffect } from "react";
@@ -16,49 +16,82 @@ import isTokenExpired from "../isTokenExpired.js";
 import decodeToken from "../decodeToken.js";
 
 const Main = () => {
-    const location = useLocation();
-    const navigate = useNavigate()
-    const isDashboard = location.pathname.includes('/dashboard') || location.pathname === ('/profile');
+  const location = useLocation();
+  const navigate = useNavigate();
+  const isDashboard =
+    location.pathname.includes("/dashboard") ||
+    location.pathname === "/profile";
 
-
-
-    
-
-    return (
-        <MainContent>
-            {isDashboard && <Sidebar />}
-            <Content $isDashboard={isDashboard}>
-                <Routes>
-                    <Route exact path="/" element={<Home />} />
-                    <Route path="/about" element={<About />} />
-                    <Route path="/dashboard" element={ <ProtectedRoute><Dashboard /></ProtectedRoute>} />
-                    <Route path="/signup" element={<SignUp />} />
-                    <Route path="/signin" element={<SignIn />} />
-                    <Route path="/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-                    <Route path="/dashboard/channels" element={<ProtectedRoute><Channels /></ProtectedRoute>} />
-                    <Route path="/dashboard/channels/:id" element={<ProtectedRoute><Channel/></ProtectedRoute>} />
-                    <Route path="dashboard/profile" element={<ProtectedRoute><Profile /></ProtectedRoute>} />
-                </Routes>
-            </Content>
-        </MainContent>
-    );
+  return (
+    <MainContent>
+      {isDashboard && <Sidebar />}
+      <Content $isDashboard={isDashboard}>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard/channels"
+            element={
+              <ProtectedRoute>
+                <Channels />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard/channels/:id"
+            element={
+              <ProtectedRoute>
+                <Channel />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="dashboard/profile"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </Content>
+    </MainContent>
+  );
 };
 
 const MainContent = styled.div`
-    display: flex;
-    flex: 1; 
+  display: flex;
+  flex: 1;
 `;
 
 const Content = styled.div`
-    flex: 1;
-    padding: 10px 30px;
-    background-color: white;
-    border-radius: ${props => props.$isDashboard ? '10px': '0px'};
-    margin: ${props => props.$isDashboard ? '0px 5px 5px -10px': '0px'};
-    & > * {
-        color: black;
-    }
-    background-color: #eeeeee;
+  flex: 1;
+  padding: 10px 30px;
+  background-color: white;
+  border-radius: ${(props) => (props.$isDashboard ? "10px" : "0px")};
+  margin: ${(props) => (props.$isDashboard ? "0px 5px 5px -10px" : "0px")};
+  & > * {
+    color: black;
+  }
+  background-color: #eeeeee;
 `;
 
 export default Main;
--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,122 +1,113 @@
 import React from "react";
 import styled from "styled-components";
- 
+
 const Home = () => {
-    // Landing Page
-    return (
-        <Container>
-            <Hero>
-                <TextSection>
-                    <Heading>
-                        Slack is where work happens
-                    </Heading>
-                    <SubHeading>
-                        Slack is a collaboration hub, where the right people and the right information come together, helping everyone get work done.
-                    </SubHeading>
-                    <CTA>
-                        <Form>
-                            <Label htmlFor='email'>
-                                <Input type="email" placeholder="Email address" />
-                                <Button>
-                                    GET STARTED
-                                </Button>
-                            </Label>
-                        </Form>
-                    </CTA>
-                </TextSection>
-            </Hero>
-            <ImageSection>
-                <Img src="landingPage.png" alt='' />
-            </ImageSection>
-        </Container>
-    );
+  // Landing Page
+  return (
+    <Container>
+      <Hero>
+        <TextSection>
+          <Heading>McChats is where discussion happens</Heading>
+          <SubHeading>
+            A Dedicated Platform for Students, TAs, and Professors to
+            Collaborate and Communicate{" "}
+          </SubHeading>
+          <CTA>
+            <Form>
+              <Label htmlFor="email">
+                <Input type="email" placeholder="Email address" />
+                <Button>GET STARTED</Button>
+              </Label>
+            </Form>
+          </CTA>
+        </TextSection>
+      </Hero>
+      <ImageSection>
+        <Img src="landingPage.png" alt="" />
+      </ImageSection>
+    </Container>
+  );
 };
 
-const Img = styled.img` // Correct the typo from styled.im to styled.img
-    width: 100%; // Adjust the width to be responsive
-    height: auto; // Maintain aspect ratio
-    max-width: 1000px; // Adjust to match the Hero section width
+const Img = styled.img`
+  // Correct the typo from styled.im to styled.img
+  width: 100%; // Adjust the width to be responsive
+  height: auto; // Maintain aspect ratio
+  max-width: 1000px; // Adjust to match the Hero section width
 `;
 
 const TextSection = styled.div`
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-    max-width: 600px;
-`
-    
-
-
-const Container = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    gap: 50px;
-    width: 100%;
-    justify-content: center;
-    align-items: stretch; // Align items in a way that they stretch to fill the container
-    padding: 10vh 0px;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  max-width: 600px;
 `;
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 50px;
+  width: 100%;
+  justify-content: center;
+  align-items: stretch; // Align items in a way that they stretch to fill the container
+  padding: 10vh 0px;
+`;
 
 const Hero = styled.section`
-    flex: 1; // Make sure it takes up equal space
-    display: flex;
-    align-items: flex-end;
-    justify-content: center;
-    flex-direction: column;
-    gap: 20px;
+  flex: 1; // Make sure it takes up equal space
+  display: flex;
+  align-items: flex-end;
+  justify-content: center;
+  flex-direction: column;
+  gap: 20px;
 `;
 
 const ImageSection = styled.section`
-    flex: 1; // Make sure it takes up equal space
-    display: flex; // Add display flex
-    align-items: center; // Center the content vertically
+  flex: 1; // Make sure it takes up equal space
+  display: flex; // Add display flex
+  align-items: center; // Center the content vertically
 `;
 
 const Heading = styled.h1`
-    font-size: 55px;
-    line-height: 45px;
-    padding-bottom: 5px;
-
-`
+  font-size: 55px;
+  line-height: 45px;
+  padding-bottom: 5px;
+`;
 
 const SubHeading = styled.h3`
-    font-size: 20px;
-    letter-spacing: 1.5px;
-    line-height: 20px;
-    padding-bottom: 10px;
-
-`
+  font-size: 20px;
+  letter-spacing: 1.5px;
+  line-height: 20px;
+  padding-bottom: 10px;
+`;
 const CTA = styled.div`
-    
-    width: 100%;
-`
+  width: 100%;
+`;
 
-const Form = styled.form`
-`
+const Form = styled.form``;
 
 const Label = styled.label`
-    display: flex;
-    flex-wrap: wrap;
-    gap: 5px;
-`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 5px;
+`;
 
 const Input = styled.input`
-    flex: 2;
-    padding: 10px 10px;
-    border-radius: 5px;
-    border: 1px solid black;
-    font-size: 16px;
-`
+  flex: 2;
+  padding: 10px 10px;
+  border-radius: 5px;
+  border: 1px solid black;
+  font-size: 16px;
+`;
 
 const Button = styled.button`
-    flex: 1;
-    background-color: var(--main-bg-color);
-    color: white;
-    border-radius: 5px;
-    font-size: 16px;
-    font-weight: bold;
-`
- 
+  flex: 1;
+  background-color: var(--main-bg-color);
+  color: white;
+  border-radius: 5px;
+  font-size: 16px;
+  font-weight: bold;
+`;
+
 export default Home;
